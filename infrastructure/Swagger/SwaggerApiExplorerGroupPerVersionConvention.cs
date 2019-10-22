aa0d4241acf8df20ec2fdd3fd9de13255cde51bf
Garib <garibo@o2.pl>
2019-10-23 00:07:53 +0200

    Added Swagger UI

---
+++ infrastructure/Swagger/SwaggerApiExplorerGroupPerVersionConvention.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Linq;
+
+namespace Swagger
+{
+    public class SwaggerApiExplorerGroupPerVersionConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            var controllerNamespace = controller.ControllerType.Namespace;
+            var apiVersion = controllerNamespace?.Split('.').Last().ToLower() ?? "";
+
+            if (string.IsNullOrEmpty(controller.ApiExplorer.GroupName))
+                controller.ApiExplorer.GroupName = apiVersion;
+        }
+    }
+}
---
+++ infrastructure/Swagger/SwaggerOptions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.OpenApi.Models;
+using System;
+
+namespace Swagger
+{
+    public class SwaggerOptions : OpenApiInfo
+    {
+        public bool Enabled { get; set; } = true;
+        public string Name { get; set; }
+        public string RoutePrefix { get; set; }
+        public bool EnableBearerAuth { get; set; }
+
+        public SecuritySchemeType SecurityScheme { get; set; }
+        public ParameterLocation ParameterLocation { get; set; }
+    }
+}
