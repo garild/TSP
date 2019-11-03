2d97e87fc44fb692d8f8e8dd67a08c558b389db0
Garib <garibo@o2.pl>
2019-11-03 17:33:09 +0100

    Nugger packing and publishing packages

---
+++ run-pack-packages-publish.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+Projects=$(dirname $0)/src/infrastructure/*
+
+for f in $Projects
+do
+    ProjectName="$(basename -- $f)"
+    tput setaf 2;echo -e "\nRestoring project name $ProjectName\n"; tput sgr0;
+	dotnet build -v q $f/$ProjectName.csproj -c Release  
+
+done
+
+Nuggets=$(dirname $0)/packages/*.nupkg
+
+for f in $Nuggets
+do
+  NuggetName="$(basename -- $f)"
+  tput setaf 2; echo -e "\nPushing package $NuggetName ...\n"; tput sgr0;
+  dotnet nuget push $f -k oy2mzu7weg4mvjvvc5palw4msksnjjyerejn3ytmuvli2e -s https://api.nuget.org/v3/index.json
+
+done
